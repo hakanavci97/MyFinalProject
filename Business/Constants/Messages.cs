b07bc8e8135d813898ae889b2c264262330c3e6d
Hakan Avci <hkn.avci@hotmail.com>
2021-04-06 23:44:31 +0300

    15.Gün

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -18,8 +18,8 @@ namespace Business.Constans
         public static string PasswordError = "Şifre hatalı";
         public static string SuccessfulLogin = "Sisteme giriş başarılı";
         public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
-        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
-        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi.";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu.";
 
       
     }
